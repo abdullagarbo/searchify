3fac8a0f35a579ae003d4e05fc0a63220e06582b
Abdulla <abdulla.garbo@gmail.com>
2025-01-11 17:28:18 +0100

    updated Header component, created results page

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { useFormStatus } from 'react-dom';
 import styles from './Button.module.css';
 
 function Button({ title }: { title: string }) {
+  const { pending } = useFormStatus();
+
   return (
-    <button type='submit' className={styles.button}>
-      {title}
+    <button type='submit' className={styles.button} disabled={pending}>
+      {pending ? 'Wait...' : title}
     </button>
   );
 }
--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image';
+import avatar from '@/public/avatar.png';
+import { TbGridDots } from 'react-icons/tb';
 import styles from './Header.module.css';
 
 function Header() {
   return (
     <header className={styles.header}>
       <p>Agile Content Frontend test</p>
-      <p>Version 0.1.0</p>
+      <div className={styles.userSection}>
+        <TbGridDots size={30} />
+        <Image
+          src={avatar}
+          alt='User Avatar'
+          width={30}
+          height={30}
+          className={styles.avatar}
+        />
+      </div>
     </header>
   );
 }
