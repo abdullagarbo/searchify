249e3e73faa6895ca31a460ea6f2b694c1200e41
Abdulla <abdulla.garbo@gmail.com>
2025-01-13 14:59:45 +0100

    updated NotFoundPage

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,19 @@
+import { Suspense } from 'react';
 import PageNotFound from '@/components/global/PageNotFound';
 
-function NotFoundPage() {
+const NotFoundPage = () => {
   return (
     <PageNotFound
       title='Page not found 404'
       message='Unfortunately we could not found that page.'
     />
   );
-}
+};
 
-export default NotFoundPage;
+const SuspendedNotFound = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <NotFoundPage />
+  </Suspense>
+);
+
+export default SuspendedNotFound;
